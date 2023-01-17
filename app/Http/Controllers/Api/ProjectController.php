d973929dabc8c34ed4434fbdbee92a8f1cb6b165
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2023-01-17 15:13:39 +0100

    esercizio milestone 1 e 2

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+class ProjectController extends Controller
+{
+    public function index(){
+        $projects = Project::with('languages', 'types')->paginate(3);
+        return response()->json([
+            'succes' => true,
+            'results' => $projects
+        ]);
+    }
+    public function show($slug){
+        $project = Project::with('languages', 'types')->where('slug', $slug)->first();
+        if($project){
+            return response()->json([
+                'succes' => true,
+                'results' => $project
+            ]);
+        }else{
+            return response()->json([
+                'succes' => false,
+                'results' => 'Nessun progetto trovato'
+            ]);
+        }
+       
+    }
+}
